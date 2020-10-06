e7a030f252510ee3ec7ad8a66535aef1737a9bfc
TrungLuong1194 <trungluong1194@gmail.com>
2020-10-07 01:55:48 +0300

    update

--- searching_algorithms/main.py
+++ searching_algorithms/main.py
@@ -4,11 +4,12 @@ from searching_algorithms.breadth_first_search import BFS
 dict_vertex = {0: 'a', 1: 'b', 2: 'c', 3: 'd', 4: 'e', 5: 'f', 6: 'g', 7: 'h', 8: 'i'}
 
 # Create graph and edges
-graph = Graph()
+graph = Graph(dict_vertex)
 
 graph.add_edge(0, 4)
 graph.add_edge(0, 6)
 graph.add_edge(1, 0)
+graph.add_edge(1, 2)
 graph.add_edge(3, 2)
 graph.add_edge(4, 0)
 graph.add_edge(4, 5)
@@ -19,7 +20,8 @@ graph.add_edge(7, 8)
 graph.add_edge(8, 7)
 graph.add_edge(8, 2)
 
-graph.print_graph(dict_vertex)
+# graph.print_graph()
+graph.get_schema()
 
 # Execute BFS
 bfs = BFS(graph.get_graph())
